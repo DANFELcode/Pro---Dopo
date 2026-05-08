00e7905d1a9c1d7630da1ed45f99e7bae74665f4
Juan Munar <juandamuch13@gmail.com>
2026-05-07 22:55:54 -0500

    Update ForestException messages for clarity

--- labs/LAB06/forest/src/domain/ForestException.java
+++ labs/LAB06/forest/src/domain/ForestException.java
@@ -1,13 +1,11 @@
 package domain;
  
 public class ForestException extends Exception {
-	
-	
-    public static final String ABRIR    = "Opción Abrir en construcción.";
-    public static final String GUARDARCOMO    = "Opción Guardar Como en construcción.";
-    public static final String IMPORTAR    = "Opción Importar en construcción.";
-    public static final String EXPORTARCOMO    = "Opción Exportar Como en construcción.";
- 
+    public static final String ABRIR = "Error al abrir";
+    public static final String GUARDARCOMO = "Error al guardar como";
+    public static final String IMPORTAR = "Error al importar"; 
+    public static final String EXPORTARCOMO = "Error al exportar como"; 
+
     public ForestException(String message) {
         super(message);
     }
